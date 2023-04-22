4f5ce4c28532839fb088ac473060f78f6511467c
Andrew Buckingham <a.d.buckingham@dunelm.org.uk>
2023-04-22 14:27:47 +0100

    Tidy

--- ChargePlan.Builder/ChargeProfile.cs
+++ ChargePlan.Builder/ChargeProfile.cs
@@ -4,10 +4,6 @@ public class ChargeProfile : IChargeProfile
 {
     public List<ChargeValue> Values = new();
 
-    public DateTime Starting => Values.Min(f => f.DateTime);
-
-    public DateTime Until => Values.Max(f => f.DateTime);
-
     public IInterpolation AsSpline<T>(Func<IEnumerable<double>, IEnumerable<double>, T> splineCreator) where T : IInterpolation
         => splineCreator(Values.Select(f => (double)f.DateTime.AsTotalHours()), Values.Select(f => (double)f.Power));
 }
--- ChargePlan.Builder/GenerationProfile.cs
+++ ChargePlan.Builder/GenerationProfile.cs
@@ -4,10 +4,6 @@ public record GenerationProfile : IGenerationProfile
 {
     public List<GenerationValue> Values = new();
 
-    public DateTime Starting => Values.Min(f => f.DateTime);
-
-    public DateTime Until => Values.Max(f => f.DateTime);
-
     public IInterpolation AsSpline<T>(Func<IEnumerable<double>, IEnumerable<double>, T> splineCreator) where T : IInterpolation
         => splineCreator(Values.Select(f => (double)f.DateTime.AsTotalHours()), Values.Select(f => (double)f.Power));
 }
