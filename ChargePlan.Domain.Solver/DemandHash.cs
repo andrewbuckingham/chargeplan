f8aefcc803ec609d12e5283279cef2e20a814735
Andrew Buckingham <a.d.buckingham@dunelm.org.uk>
2023-05-01 21:41:24 +0100

    Better hash

--- ChargePlan.Domain.Solver/DemandHash.cs
+++ ChargePlan.Domain.Solver/DemandHash.cs
@@ -1,5 +1,15 @@
+using System.Security.Cryptography;
+using System.Text;
+
 public static class ShiftableDemandProfileExtensions
 {
     public static string AsDemandHash(this IShiftableDemandProfile profile)
-        => (profile.WithinDayRange?.From, profile.WithinDayRange?.To, profile.Name).GetHashCode().ToString();
+    {
+        using var md5 = MD5.Create();
+
+        string str = $"{profile.WithinDayRange?.From.Ticks}{profile.WithinDayRange?.To.Ticks}{profile.Name}";
+
+        var data = md5.ComputeHash(Encoding.Default.GetBytes(str));
+        return BitConverter.ToString(data).Replace("-", "").Substring(0, 16);
+    }
 }
