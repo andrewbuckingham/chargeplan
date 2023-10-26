b0c055a4c1588622a2670381af1bc1d277d04d64
Andrew Buckingham <a.d.buckingham@dunelm.org.uk>
2023-10-26 20:04:55 +0100

    Beefed up exception handling

--- ChargePlan.Api/ForecastTuningFunctions.cs
+++ ChargePlan.Api/ForecastTuningFunctions.cs
@@ -20,11 +20,11 @@ public class ForecastTuningFunctions
         _service = service ?? throw new ArgumentNullException(nameof(service));
     }
 
-#if DEBUGkjkjh
+#if DEBUG
 #else
     [Function(nameof(StoreForecastHistory))]
 #endif
-    public Task StoreForecastHistory([TimerTrigger("* * * * *")]TimerInfo myTimer)
+    public Task StoreForecastHistory([TimerTrigger("0 * * * *")]TimerInfo myTimer)
         => _service.StoreForecastInHistory(new Guid(MyUserId));
 
     [Function(nameof(StoreEnergyHistory))]
--- ChargePlan.Api/HttpRequestDataExtensions.cs
+++ ChargePlan.Api/HttpRequestDataExtensions.cs
@@ -7,13 +7,49 @@ namespace ChargePlan.Api;
 
 public static class HttpRequestDataExtensions
 {
-    public static async Task<HttpResponseData> GetFromService<T>(this HttpRequestData req, ILogger logger, string name, Func<Task<T>> service)
+    private static async Task<HttpResponseData> WrapService(this HttpRequestData req, ILogger logger, string name, Func<Task<HttpResponseData>> serviceCall)
     {
-        logger.LogInformation(name);
-
+        logger.LogInformation($"Starting {name}");
         try
         {
-            T? result = await service(); //?? throw new InvalidStateException("Service returned null");
+            var response = await serviceCall();
+            return response;
+        }
+        catch (InfrastructureException ex)
+        {
+            logger.LogError(ex, $"Infrastructure exception in service {name}");
+            return req.CreateResponse(HttpStatusCode.ServiceUnavailable);
+        }
+        catch (ConcurrencyException ex)
+        {
+            logger.LogError(ex, $"Concurrency exception in service {name}");
+            return req.CreateResponse(HttpStatusCode.Conflict);
+        }
+        catch (InvalidStateException ex)
+        {
+            logger.LogError(ex, $"Invalid state exception in service {name}");
+            return req.CreateResponse(HttpStatusCode.UnprocessableEntity);
+        }
+        catch (NotAuthenticatedException ex)
+        {
+            logger.LogError(ex, $"Unauthenticated in service {name}");
+            return req.CreateResponse(HttpStatusCode.Unauthorized);
+        }
+        catch (NotPermittedException ex)
+        {
+            logger.LogError(ex, $"Not permitted in service {name}");
+            return req.CreateResponse(HttpStatusCode.Forbidden);
+        }
+        catch (Exception ex)
+        {
+            logger.LogCritical(ex, $"Failed calling service {name}");
+            return req.CreateResponse(HttpStatusCode.InternalServerError);
+        }
+    }
+
+    public static Task<HttpResponseData> GetFromService<T>(this HttpRequestData req, ILogger logger, string name, Func<Task<T>> service)
+        => req.WrapService(logger, name, async () => {
+            T? result = await service();
 
             if (result == null) return req.CreateResponse(HttpStatusCode.NotFound);
             
@@ -21,100 +57,43 @@ public static class HttpRequestDataExtensions
             await response.WriteAsJsonAsync(result);
 
             return response;
-        }
-        catch (Exception ex)
-        {
-            logger.LogError(ex, $"Failed calling service {name}");
-            var response = req.CreateResponse(HttpStatusCode.InternalServerError);
-            //return response;
-            throw;
-        }
-    }
+        });
 
-    public static async Task<HttpResponseData> UpdateWithService<T>(this HttpRequestData req, ILogger logger, string name, Func<T, Task<T>> service)
-    {
-        logger.LogInformation(name);
-
-        try
-        {
+    public static Task<HttpResponseData> UpdateWithService<T>(this HttpRequestData req, ILogger logger, string name, Func<T, Task<T>> service)
+        => req.WrapService(logger, name, async () => {
             T received = await req.ReadFromJsonAsync<T>() ?? throw new InvalidStateException("You must send some data");
             T result = await service(received) ?? throw new InvalidStateException("Service returned null");
             var response = req.CreateResponse(HttpStatusCode.OK);
             await response.WriteAsJsonAsync(result);
 
             return response;
-        }
-        catch (Exception ex)
-        {            
-            logger.LogError(ex, $"Failed calling service {name}");
-            var response = req.CreateResponse(HttpStatusCode.InternalServerError);
-            //return response;
-            throw;
-        }
-    }
-
-    public static async Task<HttpResponseData> CreateWithService<TParam, TResult>(this HttpRequestData req, ILogger logger, string name, Func<TParam, Task<TResult>> service)
-    {
-        logger.LogInformation(name);
-
-        try
-        {
+        });
+        
+    public static Task<HttpResponseData> CreateWithService<TParam, TResult>(this HttpRequestData req, ILogger logger, string name, Func<TParam, Task<TResult>> service)
+        => req.WrapService(logger, name, async () => {
             TParam received = await req.ReadFromJsonAsync<TParam>() ?? throw new InvalidStateException("You must send some data");
             TResult result = await service(received) ?? throw new InvalidStateException("Service returned null");
             var response = req.CreateResponse(HttpStatusCode.OK);
             await response.WriteAsJsonAsync(result);
 
             return response;
-        }
-        catch (Exception ex)
-        {
-            logger.LogError(ex, $"Failed calling service {name}");
-            var response = req.CreateResponse(HttpStatusCode.InternalServerError);
-            //return response;
-            throw;
-        }
-    }
+        });
 
-    public static async Task<HttpResponseData> CreateWithService<TResult>(this HttpRequestData req, ILogger logger, string name, Func<string, Task<TResult>> service)
-    {
-        logger.LogInformation(name);
-
-        try
-        {
+    public static Task<HttpResponseData> CreateWithService<TResult>(this HttpRequestData req, ILogger logger, string name, Func<string, Task<TResult>> service)
+        => req.WrapService(logger, name, async () => {
             string received = await req.ReadAsStringAsync() ?? throw new InvalidStateException("You must send some data");
             TResult result = await service(received) ?? throw new InvalidStateException("Service returned null");
             var response = req.CreateResponse(HttpStatusCode.OK);
             await response.WriteAsJsonAsync(result);
 
             return response;
-        }
-        catch (Exception ex)
-        {
-            logger.LogError(ex, $"Failed calling service {name}");
-            var response = req.CreateResponse(HttpStatusCode.InternalServerError);
-            //return response;
-            throw;
-        }
-    }
+        });
 
-    public static async Task<HttpResponseData> DeleteWithService<TParam>(this HttpRequestData req, ILogger logger, string name, Task service)
-    {
-        logger.LogInformation(name);
-
-        try
-        {
+    public static Task<HttpResponseData> DeleteWithService<TParam>(this HttpRequestData req, ILogger logger, string name, Task service)
+        => req.WrapService(logger, name, async () => {
             await service;
             var response = req.CreateResponse(HttpStatusCode.OK);
 
             return response;
-        }
-        catch (Exception ex)
-        {
-            logger.LogError(ex, $"Failed calling service {name}");
-            var response = req.CreateResponse(HttpStatusCode.InternalServerError);
-            //return response;
-            throw;
-        }
-    }
-
+        });
 }
